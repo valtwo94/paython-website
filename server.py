43163312953cea9d497d89b4f1b2662032b16ec4
성현제 <hyunjaesung@seonghyeonje.local>
2020-04-14 12:18:57 +0900

    라우트 템플릿 설정

---
+++ server.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return render_template('index.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/blog/2020/dogs')
+def blog2():
+    return 'this is my dog'
