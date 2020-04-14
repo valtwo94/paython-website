ec43a1100777bccc71ad2169d3053102b28fd9b9
성현제 <hyunjaesung@seonghyeonje.local>
2020-04-14 16:33:01 +0900

    라우이름과 파일명 동일하게 해서 간단하게 라우트 정리

--- server.py
+++ server.py
@@ -7,26 +7,7 @@ def my_home():
     return render_template('index.html')
 
 
-@app.route('/about')
-def about():
-    return render_template('about.html')
+@app.route('/<string:page_name>')
+def html_page(page_name):
+    return render_template(page_name)
 
-
-@app.route('/contact')
-def contact():
-    return render_template('contact.html')
-
-
-@app.route('/components')
-def components():
-    return render_template('components.html')
-
-
-@app.route('/work')
-def work():
-    return render_template('work.html')
-
-
-@app.route('/works')
-def works():
-    return render_template('works.html')
