524d052405c02b7fe7f3ebfbd1caec0410545332
성현제 <hyunjaesung@seonghyeonje.local>
2020-04-16 09:23:44 +0900

    contact form 자료 csv파일에 연결 확인

--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+import csv
 from flask import Flask, render_template, url_for, request, redirect
 app = Flask(__name__)
 
@@ -20,11 +21,21 @@ def write_to_file(data):
         file = database.write(f'\n{email}, {subject}, {message}')
 
 
+def write_to_csv(data):
+    with open('database.csv', mode='a', newline='') as database2:
+        email = data['email']
+        subject = data["subject"]
+        message = data["message"]
+        csv_writer = csv.writer(database2, delimiter=',',
+                                quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        csv_writer.writerow([email, subject, message])
+
+
 @app.route('/submit_form', methods=['POST', 'GET'])
 def submit_form():
     if request.method == 'POST':
         data = request.form.to_dict()
-        write_to_file(data)
+        write_to_csv(data)
         return redirect('/thankyou.html')
     else:
         return 'something went wrong try again'
