cbf1f865cd50dab1874681e2299467439d649193
성현제 <hyunjaesung@seonghyeonje.local>
2020-04-16 09:03:05 +0900

    contact data전송하는 함수 생성

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, request, redirect
 app = Flask(__name__)
 
 
@@ -11,3 +11,20 @@ def my_home():
 def html_page(page_name):
     return render_template(page_name)
 
+
+def write_to_file(data):
+    with open('database.txt', mode='a') as database:
+        email = data["email"]
+        subject = data["subject"]
+        message = data['message']
+        file = database.write(f'\n{email}, {subject}, {message}')
+
+
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_form():
+    if request.method == 'POST':
+        data = request.form.to_dict()
+        write_to_file(data)
+        return redirect('/thankyou.html')
+    else:
+        return 'something went wrong try again'
